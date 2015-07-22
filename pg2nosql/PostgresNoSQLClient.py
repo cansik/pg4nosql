6780c2d796bc0bdc0942a5bbb17e4ffdb40f2f4f
Florian <florian@nexpose.ch>
2015-07-22 13:06:34 +0200

    fixed sql injection vulnerability

--- pg2nosql/PostgresNoSQLClient.py
+++ pg2nosql/PostgresNoSQLClient.py
@@ -5,7 +5,7 @@ from PostgresNoSQLTable import PostgresNoSQLTable
 class PostgresNoSQLClient(object):
     SQL_CREATE_JSON_TABLE = 'CREATE TABLE %s (id SERIAL, data JSON);'
     SQL_DROP_JSON_TABLE = 'DROP TABLE IF EXISTS %s;'
-    SQL_TABLE_EXISTS = "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='%s')"
+    SQL_TABLE_EXISTS = "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname=%s)"
 
     def __init__(self):
         self.connection = None
@@ -19,12 +19,12 @@ class PostgresNoSQLClient(object):
         return self.connection.close()
 
     def create_table(self, table_name):
-        self.cursor.execute(self.SQL_CREATE_JSON_TABLE % table_name)
+        self.cursor.execute(self.SQL_CREATE_JSON_TABLE, (table_name,))
         self.commit()
         return PostgresNoSQLTable(table_name, self.connection)
 
     def drop_table(self, table_name):
-        self.cursor.execute(self.SQL_DROP_JSON_TABLE % table_name)
+        self.cursor.execute(self.SQL_DROP_JSON_TABLE, (table_name,))
         self.commit()
 
     def get_table(self, table_name):
@@ -39,7 +39,7 @@ class PostgresNoSQLClient(object):
     def table_exists(self, table_name):
         exists = False
         try:
-            self.cursor.execute(self.SQL_TABLE_EXISTS % table_name)
+            self.cursor.execute(self.SQL_TABLE_EXISTS, (table_name,))
             exists = self.cursor.fetchone()[0]
         except psycopg2.Error as e:
             print e
--- pg2nosql/PostgresNoSQLTable.py
+++ pg2nosql/PostgresNoSQLTable.py
@@ -1,10 +1,11 @@
 import json
 import psycopg2
+from psycopg2.extensions import AsIs
 
 
 class PostgresNoSQLTable(object):
 
-    SQL_INSERT_JSON = "INSERT INTO %s(data) VALUES('%s') RETURNING id"
+    SQL_INSERT_JSON = "INSERT INTO %s(data) VALUES(%s) RETURNING id"
     SQL_QUERY_JSON = 'SELECT %s FROM %s WHERE %s'
     SQL_GET_JSON = 'SELECT * FROM %s WHERE id=%s'
 
@@ -17,15 +18,15 @@ class PostgresNoSQLTable(object):
         self.connection.commit()
 
     def put(self, data):
-        self.cursor.execute(self.SQL_INSERT_JSON % (self.name, json.dumps(data)))
+        self.cursor.execute(self.SQL_INSERT_JSON, (AsIs(self.name), json.dumps(data)))
         return self.cursor.fetchone()[0]
 
     def get(self, id):
-        self.cursor.execute(self.SQL_GET_JSON % (self.name, id))
+        self.cursor.execute(self.SQL_GET_JSON, (AsIs(self.name), id))
         return self.cursor.fetchone()
 
     def query(self, query='True', columns='*'):
-        self.cursor.execute(self.SQL_QUERY_JSON % (columns, self.name, query))
+        self.cursor.execute(self.SQL_QUERY_JSON, (AsIs(columns), AsIs(self.name), AsIs(query)))
         rows = [item for item in self.cursor.fetchall()]
         return rows
 
