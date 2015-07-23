0bcfafebb63ac21867fc96c25a88850cfd150f65
Florian <florian@nexpose.ch>
2015-07-23 13:29:22 +0200

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pg2nosql',
+    version='0.0.1',
+    packages=['pg4nosql'],
+    url='https://github.com/cansik/pg4nosql',
+    license='MIT License',
+    author='cansik',
+    author_email='florian@nexpose.ch',
+    description='A simple psycopg2 based wrapper for nosql like database interaction with python.'
+)
