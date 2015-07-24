3b298c494763601f97f57ac3cdf54369162cfc0d
Florian <florian@nexpose.ch>
2015-07-24 10:14:23 +0200

    prepared pip release

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     packages=['pg4nosql'],
     url='https://github.com/cansik/pg4nosql',
     license='MIT License',
-    author='cansik',
+    author='Florian Bruggisser',
     author_email='florian@nexpose.ch',
-    description='A simple psycopg2 based wrapper for nosql like database interaction with python.'
+    description='A simple psycopg2 based wrapper for nosql like database interaction with python.',
+    download_url='https://github.com/cansik/pg4nosql/tarball/0.1.1',
+    keywords=['postgres', 'nosql', 'wrapper', 'database', 'json'],
+    classifiers=[],
 )
