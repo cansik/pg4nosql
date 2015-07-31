289569218beee86bf915f66066918182ccfdd7b1
Florian <florian@nexpose.ch>
2015-07-31 19:18:55 +0200

    travis

--- test/__init__.py
+++ test/__init__.py
@@ -4,12 +4,13 @@ TEST_DB_HOST = 'localhost' if 'TRAVIS' not in os.environ else os.environ['DB_HOS
 TEST_DB_USER = None if 'TRAVIS' not in os.environ else os.environ['DB_USER']
 TEST_DB_PASSWORD = None if 'TRAVIS' not in os.environ else os.environ['DB_PASSWORD']
 
-TEST_DATABASE = 'pg4nosql_test' + ('' if 'TRAVIS' not in os.environ else os.environ['TRAVIS_PYTHON_VERSION']).replace(
-    '.', '_')
+TEST_PYTHON_VERSION = ('' if 'TRAVIS' not in os.environ else os.environ['TRAVIS_PYTHON_VERSION']).replace('.', '_')
+
+TEST_DATABASE = 'pg4nosql_test' + TEST_PYTHON_VERSION
 TEST_TABLE = 'pg4nosql_table'
 TEST_RELATIONAL_TABLE = 'pg4nosql_rel_test'
 
-TEST_TEMP_DATABASE = 'temp_db'
+TEST_TEMP_DATABASE = 'temp_db' + TEST_PYTHON_VERSION
 TEST_TEMP_TABLE = 'temp_table'
 
 RELATIONAL_FIELDS = dict(name='text', age='integer NOT NULL', height='real')
