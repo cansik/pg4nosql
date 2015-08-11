c6ec075ec00bb957378e8e30ae7c507c3564b070
Florian <florian@nexpose.ch>
2015-08-11 14:34:00 +0200

    implemented execute on database object #fixes 17

--- pg4nosql/PostgresNoSQLDatabase.py
+++ pg4nosql/PostgresNoSQLDatabase.py
@@ -5,7 +5,6 @@ from pg4nosql.PostgresNoSQLTable import PostgresNoSQLTable
 
 
 class PostgresNoSQLDatabase(object):
-
     __SQL_CREATE_JSON_TABLE = 'CREATE TABLE %s (id SERIAL PRIMARY KEY %s, ' + DEFAULT_JSON_COLUMN_NAME + ' JSON);'
     __SQL_DROP_JSON_TABLE = 'DROP TABLE IF EXISTS %s;'
     __SQL_TABLE_EXISTS = "SELECT EXISTS(SELECT relname FROM pg_class WHERE relname=%s)"
@@ -20,6 +19,10 @@ class PostgresNoSQLDatabase(object):
 
     def commit(self):
         self.connection.commit()
+
+    def execute(self, sql_query):
+        self.cursor.execute(sql_query)
+        return self.cursor.fetchall()
 
     def create_table(self, table_name, **relational_columns):
         # create additional columns string
