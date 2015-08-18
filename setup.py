2fba2e7b60edca0e9420d77ff7533721dd571147
Florian <florian@nexpose.ch>
2015-08-18 11:34:38 +0200

    rst documentation for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from distutils.core import setup
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = ''
+
 setup(
     name='pg4nosql',
-    version='0.3.2',
+    version='0.3.3',
     packages=['pg4nosql'],
     url='https://github.com/cansik/pg4nosql',
     license='MIT License',
     author='Florian Bruggisser',
     author_email='florian@nexpose.ch',
     description='A simple psycopg2 based wrapper for nosql like database interaction with python.',
-    long_description=open('README.md').read(),
-    download_url='https://github.com/cansik/pg4nosql/tarball/0.3.2',
+    long_description=description,
+    download_url='https://github.com/cansik/pg4nosql/tarball/0.3.3',
     keywords=['postgres', 'nosql', 'wrapper', 'database', 'json'],
     classifiers=[
         'Programming Language :: Python',
